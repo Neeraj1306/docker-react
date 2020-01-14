fc254bcec3c84e3b936c851c411d3ae7e8c8a484
Neosoft <neerajgk@lentra.ai>
2020-01-14 18:01:39 +0530

    changed app text in feature branch

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload3.
         </p>
+        <p>I was changed on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
